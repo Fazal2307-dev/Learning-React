8a4af858437683406ea029fbfac85805f5cc2787
Fazal2307-dev <mdfazal2307@gmail.com>
2025-03-21 21:36:05 +0530

    commit

--- index.js
+++ index.js
@@ -41,27 +41,55 @@ const HeaderComponent = () =>{
                     <li>Cart</li>
                     
                 </ul>
-
-            
-            
             </div>        
-      
         </div>
-    
 )};
+
+const burgerKing ={
+    name:"Burger King",
+    image:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/4/22/0438b481-ff18-48f2-b8c5-56297101c40f_8eab0d79-e411-4ff6-9b85-4b02aa637cd9.jpg",
+    cusines:["Burger,American"],
+    rating:"4.2", 
+}
+
+const RestaurntCard = () =>{
+    return(
+        <div className="card">
+            <img  alt ="Res Card" src={burgerKing.image}></img>
+            <h2>{burgerKing.name}</h2>
+            <h3>{burgerKing.cusines.join(" , ")}</h3>
+            <h4>{burgerKing.rating}</h4>
+            
+
+        </div>
+    )
+}
 const Body =() =>{
     return (
-        <div>
-            <h3>
-                Body
-            </h3>
+        <div className="body">
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
+            <RestaurntCard />
         </div>
 
     )
 }
 const Footer =() =>{
     return (
-        <div>
+        <div className="footer">
             <h3>
                Footer
             </h3>
