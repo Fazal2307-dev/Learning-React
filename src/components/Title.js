e558d11ac30f2c62d84fb20a0adfde14c7bb3141
Fazal2307-dev <mdfazal2307@gmail.com>
2025-03-23 23:38:41 +0530

    adding file structure  in react project

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+const Title = () => (
+    <a href="/">
+<img  className="logo"
+alt="logo" 
+src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS51Fg8ECaMCb6T2yAd4CgUjy_hu7Zk1OKEZw&s">
+</img>
+</a>
+);
+
+export default Title;
