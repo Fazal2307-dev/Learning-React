2319e0bd9d546febc176910c7a687d558a6457e9
Fazal2307-dev <mdfazal2307@gmail.com>
2025-03-24 00:15:00 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+//default import
 import Header from "./components/Header";
+// named  import
+import {Title} from "./components/Header";
 
  /**
      * Header
