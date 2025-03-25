8198e8b4e4a1d6a7a8c333623071eb196dd70e40
Fazal2307-dev <mdfazal2307@gmail.com>
2025-03-25 21:03:05 +0530

    new folder added

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+
+
+const Footer =() =>{
+    return (
+        <div className="footer">
+            <h3>
+               Footer
+            </h3>
+        </div>
+
+    )
+}
+export default Footer;
